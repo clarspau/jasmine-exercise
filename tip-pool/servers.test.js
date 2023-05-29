44778fc2c6cfcdd0638655a734af6b065a0e2bff
clarspau <125636195+clarspau@users.noreply.github.com>
2023-05-28 17:27:20 -0700

    creating servers.test.js

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -4,6 +4,7 @@ describe("Servers test (with setup and tear-down)", function() {
     serverNameInput.value = 'Alice';
   });
 
+  
   it('should add a new server to allServers on submitServerInfo()', function () {
     submitServerInfo();
 
@@ -11,7 +12,38 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+
+  it('should be able to add very long server names', function() {
+    serverNameInput.value = '';
+    serverNameInput.value = 'Bruce Wayne aka Batman';
+    submitServerInfo();
+
+    expect(allServers['server' + serverId].serverName).toEqual('Bruce Wayne aka Batman');
+  });
+
+
+  it('should not add an empty input on submitServerInfo()', function() {
+    serverNameInput.value = '';
+    submitServerInfo();
+    expect(Object.keys(allServers).length).toEqual(0);
+  });
+
+
+  it('should update the #servertable on updateServerTable()', function() {
+    submitServerInfo();
+    updateServerTable();
+    let currentTable = document.querySelectorAll('#serverTable tbody tr td');
+
+    expect(currentTable.length).toEqual(2);
+    expect(currentTable[0].innerText).toEqual('Alice');
+    expect(currentTable[1].innerText).toEqual('$0.00');
+  })
+
+
+// To clean up the dom after running the tests
   afterEach(function() {
-    // teardown logic
+    allServers = {};
+    serverId = 0;
+    serverTbody.innerHTML = '';
   });
 });
